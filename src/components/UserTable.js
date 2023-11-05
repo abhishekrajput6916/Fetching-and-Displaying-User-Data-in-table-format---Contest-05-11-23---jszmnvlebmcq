d297bb80786d3c0d56efd27df7816720f055de79
Abhishek Rajput <98444175+abhishekrajput6916@users.noreply.github.com>
2023-11-05 19:35:14 +0530

    Update UserTable.js

--- src/components/UserTable.js
+++ src/components/UserTable.js
@@ -1,5 +1,30 @@
 import React, { useState, useEffect } from "react";
 
-function UserTable() {}
+function UserTable() {
+  const [data, setData] = useState();
+  async function getData() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    console.log(data);
+    setData(() => {
+      return data.map((entry) => {
+        return (
+          <div className="item">
+            <div key={entry.id}>{entry.id}</div>
+            <div key={entry.id}>{entry.name}</div>
+            <div key={entry.id}>{entry.email}</div>
+            <div key={entry.id}>{entry.divhone}</div>
+            <div key={entry.id}>{entry.website}</div>
+          </div>
+        );
+      });
+    });
+  }
 
+  useEffect(() => {
+    getData();
+  }, []);
+  return <div className="container">{data}</div>;
+}
+//https://jsonplaceholder.typicode.com/users
 export default UserTable;
