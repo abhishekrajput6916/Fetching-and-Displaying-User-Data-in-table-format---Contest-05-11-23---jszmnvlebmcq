cd26887c6239431612ee9ee8678d398435bd711b
Abhishek Rajput <98444175+abhishekrajput6916@users.noreply.github.com>
2023-11-05 19:35:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from "react";
+// import "./styles.css";
 import "../styles/App.css";
 import UserTable from "./UserTable";
 const App = () => {
-	return (
-		<div id="main">
-			<UserTable />;
-		</div>
-	);
+  return (
+    <div id="main">
+      <UserTable />
+    </div>
+  );
 };
 
 export default App;
